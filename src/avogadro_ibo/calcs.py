4d71b829e2088a20142fb11085fc87f2ec183ea4
Billy Wayne McCann <thebillywayne@gmail.com>
2026-07-02 19:05:39 -0400

    Extend _ELEM_SYMBOLS to cover all elements through I (Z=53)
    Previously the symbol table ended at Ar (Z=18), causing Br, I, and
    other common elements to appear as 'E35', 'E53' etc. in the analysis
    table.  Now covers H through I (54 entries, index 0 = placeholder).
    
    Tested: all 9 CLI tests pass.

--- src/avogadro_ibo/calcs.py
+++ src/avogadro_ibo/calcs.py
@@ -280,6 +280,10 @@ def _resolve_on_atom_mixing(C_occ, atom_of, F_IAO, dom_threshold=0.99):
 _ELEM_SYMBOLS = [
     "X", "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
     "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar",
+    "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+    "Ga", "Ge", "As", "Se", "Br", "Kr",
+    "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd",
+    "In", "Sn", "Sb", "Te", "I",
 ]
 
 
